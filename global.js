04b2ca795b6a7d9fdfd0753cd247831b7e7a2ce7
Sierra <sierramroberts.96@gmail.com>
2017-02-21 14:40:30 -0500

    Spaceships

--- global.js
+++ global.js
@@ -8,59 +8,58 @@ var spaceship1 = {
 }
 
 // Call the drive method with a speed argument value of 65
-spaceship1.fly(2)
+spaceship1.launch(2)
 
 
 
 
 
-// Object New
-var car = new Object()
-car.make = 'Toyota'
-car.model = 'Sienna'
-car.drive = function(speed) {
-    console.log(this.make + ' ' + this.model + ' is driving ' + speed + 'mph.')
+// // Object New
+var spaceship2 = new Object()
+spaceship2.make = 'Astronomical'
+spaceship2.model = 'Corona'
+spaceship2.launch = function(speed) {
+    console.log(this.make + ' ' + this.model + ' launching to infinity and beyond at ' + speed + ' lyph.')
 }
 
-// Call the drive method with a speed argument value of 35
-car.drive(35)
+// // Call the drive method with a speed argument value of 35
+spaceship2.launch(20)
 
 
 
 
 
-// Constructor Function
+// // Constructor Function
 var Vehicle = function() {
-    this.make = 'Toyota'
-    this.model = '4Runner'
-    this.drive = function(speed) {
-        console.log(this.make + ' ' + this.model + ' is driving ' + speed + 'mph.')
+    this.make = 'Ecliptic'
+    this.model = 'Flare'
+    this.launch = function(speed) {
+        console.log(this.make + ' ' + this.model + ' blasting off at  ' + speed + 'lyph.')
     }
 }
 
-var car = new Vehicle()
+var spaceship3 = new Vehicle()
 
-// Call the drive method with a speed argument value of 85
-car.drive(85)
-
-
+// // Call the drive method with a speed argument value of 85
+spaceship3.launch(45)
 
 
 
 
-// Class
-class Vehicle {
+
+
+// // Class
+class Spaceship {
     constructor() {
-        this.make = 'Toyota'
-        this.model = 'Yaris'
+        this.make = 'Celestial'
+        this.model = 'Phase'
     }
-
-    drive(speed) {
-        console.log(this.make + ' ' + this.model + ' is driving ' + speed + 'mph.')
+    launch(speed) {
+        console.log(this.make + ' ' + this.model + ' blasting away at ' + speed + ' lyph.')
     }
 }
 
-var car = new Vehicle()
+var spaceship4 = new Spaceship()
 
-// Call the drive method with a speed argument value of 55
-car.drive(55)
+// // Call the drive method with a speed argument value of 55
+spaceship4.launch(86)
